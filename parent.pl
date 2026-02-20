4ed9d871c61ec988a7577c965c47cedf729e167c
gveal <gveal07@outlook.com>
2026-02-20 14:35:41 +0000

    added comments to clarify the file

--- parent.pl
+++ parent.pl
@@ -1,5 +1,11 @@
-parent(alice, bob).
-parent(bob, carol).
+parent(alice, bob). % Alice is the parent of Bob
+parent(bob, carol). % Bob is the parent of Carol
+
+/*
+X is a grandparent of Y if
+X is a parent of Z
+and Z is a parent of Y
+*/
 
 grandparent(X, Y) :-
     parent(X, Z),
